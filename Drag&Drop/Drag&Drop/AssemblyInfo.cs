3fda39935e86a1db89a604b44cc76aebcde339b5
TheAzack9 <TheAzack9@users.noreply.github.com>
2016-04-26 23:54:19 +0200

    First commit

---
+++ Drag&Drop/Drag&Drop/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCopyright("© 2016 - theAzack9")]
+[assembly: AssemblyVersion("2.0.0.0")]
+
+// Do not change the entries below!
+#if X64
+[assembly: AssemblyInformationalVersion("3.0.2.2161 (64-bit)")]
+#else
+[assembly: AssemblyInformationalVersion("3.0.2.2161 (32-bit)")]
+#endif
+[assembly: AssemblyProduct("Rainmeter")]
---
+++ Drag&Drop/Drag&Drop/RainmeterWindowWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DragNDrop
+{
+    class RainmeterWindowWrapper : NativeWindow
+    {
+
+        
+        private IntPtr window;
+        public Action<Message> Callback = null;
+
+
+        public RainmeterWindowWrapper(IntPtr window)
+        {
+            this.window = window;
+            //When plugin is loaded, create hook
+            AssignHandle(window);
+        }
+
+
+        internal void Release()
+        {
+            // Plugin is disposed, release hook.
+            ReleaseHandle();
+        }
+
+        [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+        protected override void WndProc(ref Message m)
+        {
+            // Listen for operating system messages
+
+            if (Callback != null)
+                Callback(m);
+
+            base.WndProc(ref m);
+        }
+       
+    }
+}
